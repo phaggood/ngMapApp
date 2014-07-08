861eac911046ee84c2d8558167d616eef0908ae2
Patrick B Haggood <phaggood@umich.edu>
2014-07-08 14:57:40 -0400

    geolocation

--- js/controllers.js
+++ js/controllers.js
@@ -1,8 +1,16 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope,$cordovaDevice) {
+.controller('DashCtrl', function($scope,$cordovaDevice,$cordovaGeolocation) {
+        $scope.cpos = {}
         $scope.device = $cordovaDevice.getDevice();
         $scope.uuid = $cordovaDevice.getUUID();
+        $cordovaGeolocation.getCurrentPosition().then(function(position) {
+            // Position here: position.coords.latitude, position.coords.longitude
+            $scope.cpos = position;
+        }, function(err) {
+            // error
+        });
+
 
 })
 
